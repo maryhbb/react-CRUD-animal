14747c7007c2c57ffedfc8b324e97fec7dc7e467
Mary <76174247+maryhbb@users.noreply.github.com>
2023-08-26 23:45:49 +0200

    Show hint if not animals have been added

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -4,24 +4,29 @@ export default function List({ animals, onDeleteTag }) {
   return (
     <>
       <h2>Your animals:</h2>
-      <ul className="animal-list">
-        {animals.map((animal) => (
-          <li key={animal.id} className="animal-list__item">
-            <h3>{animal.name}</h3>
-            <span aria-label="emoji" className="animal-list__emoji">
-              {animal.emoji}
-            </span>
-            <button
-              className="animal-list__item-button"
-              type="button"
-              aria-label="delete list item"
-              onClick={() => onDeleteTag?.(animal)}
-            >
-              ✕
-            </button>
-          </li>
-        ))}
-      </ul>
+
+      {animals.length ? (
+        <ul className="animal-list">
+          {animals.map(animal => (
+            <li key={animal.id} className="animal-list__item">
+              <h3>{animal.name}</h3>
+              <span aria-label="emoji" className="animal-list__emoji">
+                {animal.emoji}
+              </span>
+              <button
+                className="animal-list__item-button"
+                type="button"
+                aria-label="delete list item"
+                onClick={() => onDeleteTag?.(animal)}
+              >
+                ✕
+              </button>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        "You have no animals yet"
+      )}
     </>
   );
 }

95f5a3014ce93acc90b9c0f01d1ce95707756974
Mary <76174247+maryhbb@users.noreply.github.com>
2023-08-26 21:11:40 +0200

    replace CRA with vanilla webpack

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./styles.css";
-import Form from "./components/Form/index.js";
-import List from "./components/List/index.js";
+import Form from "./components/Form/index";
+import List from "./components/List/index";
 import { uid } from "uid";
 
 const initialAnimals = [
